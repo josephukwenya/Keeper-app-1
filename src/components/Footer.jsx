27126999d9968ce892f20387e507f83f22714d91
Joseph Ukwenya <54073881+josephukwenya@users.noreply.github.com>
2022-12-30 21:15:23 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+var date = new Date();
+var currentYear = date.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {currentYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
